d89d4f48ddf99c5be7cfb86248153e08fef8d795
Artur1to <artur_gaffanov@mail.ru>
2026-07-11 19:07:30 +0500

    Изменение в SEO

--- alttools/settings.py
+++ alttools/settings.py
@@ -130,7 +130,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-SITE_URL = os.getenv('SITE_URL', 'http://127.0.0.1:8000')
+SITE_URL = os.getenv('SITE_URL', 'http://127.0.0.1:8000').rstrip('/')
 YANDEX_METRICA_ID = os.getenv('YANDEX_METRICA_ID', '')
 
 # Static files (CSS, JavaScript, Images)
--- tools/context_processors.py
+++ tools/context_processors.py
@@ -2,6 +2,11 @@ from django.conf import settings
 
 
 def yandex_metrica(request):
+    site_url = getattr(settings, 'SITE_URL', '').rstrip('/')
+    current_path = getattr(request, 'path', '/') or '/'
+
     return {
         'YANDEX_METRICA_ID': getattr(settings, 'YANDEX_METRICA_ID', ''),
+        'SITE_URL': site_url,
+        'CANONICAL_URL': f'{site_url}{current_path}',
     }
